ca27131ab2c3c17c4dd966b263b336a8b7c35481
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-27 15:06:46 +0100

    added demo script for linux

---
+++ demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gradex-overlay./test/layout.svg mark demo.pdf
+gradex-overlay./test/layout.svg moderate-active demo-mark.pdf
+gradex-overlay./test/layout.svg moderate-inactive demo-mark.pdf
+gradex-overlay./test/layout.svg check demo-mark-moderate-active.pdf
+gradex-overlay./test/layout.svg check demo-mark-moderate-inactive.pdf
+ gradex-overlay./test/layout.svg moderate-active comment-mark.pdf  
